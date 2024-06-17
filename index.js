e6b7a7495498b8d3ec780d3faf6d067089ef77e9
samuelribeiroo <samuelribeirodev@gmail.com>
2024-06-17 20:41:36 -0300

    feat(add): fetch data, select options and display price

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import clear from "clear";
+import chalk from "chalk";
+import figlet from "figlet";
+import inquirer from "inquirer";
 import { API_URL } from "./services/api.js";
 
 const log = console.log;
@@ -14,8 +18,59 @@ const formatCurrency = price => {
   return formatBRL;
 };
 
+// Async function with error handling to get latest Bitcoin Price. Using axios to fetch data.
 
-const response = await axios.get(API_URL)
-const price = response.data.bitcoin.brl
+async function getLastestBtcPrice() {
+  try {
+    const responseData = await axios.get(API_URL);
+    const lastestPrice = await responseData.data.bitcoin.brl;
+    return lastestPrice;
+  } catch (error) {
+    log("Failed Request.", error);
+  }
+}
 
-log(formatCurrency(price))
+// Function to display price, using some libs to stylized
+
+async function dipslayPrice() {
+  clear();
+  log(
+    chalk.yellow(
+      figlet.textSync("Bitcoin Tracker", {
+        font: "Standard",
+      })
+    )
+  );
+
+  const price = await getLastestBtcPrice();
+  // Return the price already formatted in BRL patterns
+
+  if (price) {
+    log(chalk.green(`Atual preço do Bitcoin: ${formatCurrency(price)}`));
+  } else {
+    log(chalk.red("Failed to fetch Bitcoin Price"));
+  }
+}
+
+const startApp = async () => {
+  // Give all options to user select answers.
+
+  const displayAnswersOptions = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "Selecione uma das opções abaixo.",
+      choices: ["Obter preço atual", "Sair"],
+    },
+  ]);
+
+  // Condicional to show results
+
+  if (displayAnswersOptions.action === "Obter preço atual") {
+    dipslayPrice();
+  } else {
+    log(chalk.yellow("Finalizado."));
+  }
+};
+
+startApp();
